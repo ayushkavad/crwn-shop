b745da38bf133c10c1bd7aeb69c226d4dc6b9b76
Ayush Kavad <69857046+ayushkavad@users.noreply.github.com>
2022-08-10 23:23:02 +0530

    updating directory page with directory.component and category.item.component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from './components/directory.component/directory.component';
+import './categories.styles.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const categories = [
+  {
+    id: 1,
+    title: 'hets',
+    imgUrl: 'https://i.ibb.co/cvpntL1/hets.png',
+  },
+  {
+    id: 2,
+    title: 'jackets',
+    imgUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+  },
+  {
+    id: 3,
+    title: 'sneakers',
+    imgUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+  },
+  {
+    id: 4,
+    title: 'womens',
+    imgUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+  },
+  {
+    id: 5,
+    title: 'mens',
+    imgUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
+  },
+];
+
+const App = () => {
+  return <Directory categories={categories} />;
+};
 
 export default App;
---
+++ src/components/directory.component/directory.component.jsx
@@ -0,0 +1,15 @@
+import CategoryItem from '../catagory.item.component/catagory.item.component';
+import './directory.styles.scss';
+
+const Directory = ({ categories }) => {
+  console.log(categories);
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
